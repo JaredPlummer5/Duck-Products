9bf6002aa5d08698df80352562f2c9c67e09335e
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-03-17 15:07:38 -0500

    I changed the pic method by appending the random pictures in to a div that is displayed because toggle was get rid of my picture if that two of the random number were the same

--- apps.js
+++ apps.js
@@ -6,10 +6,11 @@ let DisplayDiv = document.querySelector("#DisplayImagesDiv");
 
 let choosenPic = []
 let PicGenerator = function () {
-    this.clicks = 0
+    this.clicks = 0;
     this.pic = function () {
         for(let i = 0; i < 3; i++){
             let randomIndex = Math.ceil(Math.random() * 15);
+
             console.log(randomIndex)
             for(let k = 0; k < imagesSection.length; k++){
                 //console.log("You're here");
