c4e28eacbe6b147faff77c7ab22ee1417df103e9
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-03-18 20:22:47 -0500

    I got my code to change the index if I get two values that are the same in my choosenPic array

--- apps.js
+++ apps.js
@@ -1,29 +1,61 @@
 let imagesSection = document.querySelectorAll(".Images");
-console.log(imagesSection);
+//console.log(imagesSection);
 let ImageDiv = document.querySelector("#RegularImagesDiv");
 let DisplayDiv = document.querySelector("#DisplayImagesDiv");
+
+// const imagesPath = "./assets";
+// const imageNames = ["bag.jpg", "banana.jpg", "assets/bathroom.jpg"];
+// const images = [];
+
+// for (let i = 0; i < imageNames.length; i++) {
+//   const image = new Image();
+//   image.src = `${imagesPath}/${imageNames[i]}`;
+//   images.push(image);
+// }
+
+// use the images array to work with the loaded images
 
 
 let choosenPic = []
 let PicGenerator = function () {
-    this.clicks = 0;
-    this.pic = function () {
-        for(let i = 0; i < 3; i++){
+    this.timesSeen = 0;
+    //this.filePath = this.filePath;
+    //this.productName = productName
+    this.GetRandomPicIndex = function () {
+        for (let i = 0; i < 3; i++) {
             let randomIndex = Math.ceil(Math.random() * 15);
+            choosenPic.push(randomIndex);
+            //console.log(choosenPic)
+            //return randomIndex;
+        }
+    }
+    this.pic = function (randomIndex) {
+        for (let k = 0; k < imagesSection.length; k++) {
 
-            console.log(randomIndex)
-            for(let k = 0; k < imagesSection.length; k++){
-                //console.log("You're here");
-                if(k === randomIndex){
-                    DisplayDiv.append(imagesSection[k]);
-                    console.log("this is the class toggle", imagesSection[k]);
+
+            for (let i = 0; i < 3; i++) {
+                if (choosenPic[0] == choosenPic[1] || choosenPic[1] == choosenPic[2] || choosenPic[2] == choosenPic[0]) {
+                    choosenPic = []
+                    this.GetRandomPicIndex(randomIndex);
+                    console.log(choosenPic);
+                    //return choosenPic;
+                    // let newrandomIndex = Math.ceil(Math.random() * 15)
+                    // // choosenPic.push(newrandomIndex)
+                    // console.log("Two number are the same!!!", choosenPic);
+                    // choosenPic[i] = newrandomIndex;
                 }
+            }
+            if (k === randomIndex || k === randomIndex) {
+                DisplayDiv.append(imagesSection[k]);
+                console.log("this is the class of the Display div", imagesSection[k]);
             }
             
         }
-
+        
     }
+    
 }
+
 let PopUpPic = new PicGenerator()
 PopUpPic.pic();
 
