61b062697e3508c570968137e9c0ae25d9bbc082
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-03-20 15:29:41 -0500

    I started over and got the code to work

--- apps.js
+++ apps.js
@@ -3,72 +3,70 @@ let DisplayDiv = document.querySelector("#DisplayImagesDiv");
 
 timesClicked = 0;
 let choosenPic = []
-let DuckProduct = function () {
-    this.RenderPics = function () {
-        this.imagesPath = "./assets";
-        this.imageNames = ["bag.jpg", "banana.jpg", "bathroom.jpg", "boots.jpg", "breakfast.jpg", "bubblegum.jpg", "chair.jpg", "cthulhu.jpg", "dog-duck.jpg", "dragon.jpg", "pen.jpg", "pet-sweep.jpg", "scissors.jpg", "shark.jpg", "sweep.png", "tauntaun.jpg", "unicorn.jpg", "water-can.jpg", "wine-glass.jpg"];
-        this.images = [];
-        this.timesSeen = 0;
+let imageNamesUrl = ["bag.jpg", "banana.jpg", "bathroom.jpg", "boots.jpg", "breakfast.jpg", "bubblegum.jpg", "chair.jpg", "cthulhu.jpg", "dog-duck.jpg", "dragon.jpg", "pen.jpg", "pet-sweep.jpg", "scissors.jpg", "shark.jpg", "sweep.png", "tauntaun.jpg", "unicorn.jpg", "water-can.jpg", "wine-glass.jpg"];
+let imageNames = []
+let images = [];
 
-        for (let i = 0; i < this.imageNames.length; i++) {
-            let imgTag = document.createElement('img');
-            let fullImageTagPath = `${this.imagesPath}/${this.imageNames[i]}`;
-            imgTag.setAttribute("src", fullImageTagPath);
-            this.images.push(imgTag);
+function imgTagGenerator() {
 
-        }
-
+    let imagesPath = "./assets";
+    for (let i = 0; i < imageNames.length; i++) {
+        let imgTag = document.createElement('img');
+        let fullImageTagPath = `${imagesPath}/${imageNames[i]}`;
+        imgTag.setAttribute("src", fullImageTagPath);
+        images.push(imgTag);
 
     }
-    this.GetRandomPicIndex = function () {
-        choosenPic = [];
-        while (choosenPic.length < 3) {
-            let randomIndex = Math.ceil(Math.random() * 18);
-            //This checks if the randomIndex is already in the choosenPic array. 
-            //If it is not already in the array, the code inside the if statement will execute.
-            if (!choosenPic.includes(randomIndex)) {
-                // .includes() method is used to check if an array includes a certain value, 
-                // and returns a boolean value (true or false) 
-                // to indicate whether the value is found or not.
+}
+let DuckProduct = function (productName) {
+    this.timesClicked = 0;
+    this.timesSeen = 0;
+    this.productName = productName;
 
-                choosenPic.push(randomIndex);
-            }
+}
+
+function  ifItHasTheSamePicIndexAndRenderPic (randomIndex) {
+
+    DisplayDiv.innerHTML = "";
+    for (let i = 0; i < 3; i++) {
+        if (choosenPic[0] == choosenPic[1] || choosenPic[1] == choosenPic[2] || choosenPic[2] == choosenPic[0]) {
+            choosenPic.splice(0, choosenPic.length);
+            GetRandomPicIndex(randomIndex);
+            console.log(choosenPic);
         }
-    }
-
-
-    this.ifItHasTheSamePicIndexAndRenderPic = function (randomIndex) {
-        
-        DisplayDiv.innerHTML = "";
-        for (let i = 0; i < 3; i++) {
-            if (choosenPic[0] == choosenPic[1] || choosenPic[1] == choosenPic[2] || choosenPic[2] == choosenPic[0]) {
-                choosenPic.splice(0, choosenPic.length);
-                this.GetRandomPicIndex(randomIndex);
-                console.log(choosenPic);
-            }
-            DisplayDiv.append(this.images[choosenPic[i]]);
-            console.log(this.timesSeen);
-            this.timesSeen++
-        }
+        DisplayDiv.append(images[choosenPic[i]]);
        
-        
-        }
-        
-        
     }
-    
 
 
-let PopUpPic = new DuckProduct()
-PopUpPic.GetRandomPicIndex();
-PopUpPic.RenderPics();
-PopUpPic.ifItHasTheSamePicIndexAndRenderPic();
+}
+function GetRandomPicIndex () {
+    choosenPic = [];
+    while (choosenPic.length < 3) {
+        let randomIndex = Math.ceil(Math.random() * 18);
+        //This checks if the randomIndex is already in the choosenPic array. 
+        //If it is not already in the array, the code inside the if statement will execute.
+        if (!choosenPic.includes(randomIndex)) {
+            // .includes() method is used to check if an array includes a certain value, 
+            // and returns a boolean value (true or false) 
+            // to indicate whether the value is found or not.
+
+            choosenPic.push(randomIndex);
+        }
+    }
+}
+
+for (let j = 0; j < imageNamesUrl.length; j++) {
+    let bag = new DuckProduct(imageNamesUrl[j].split(".")[0], );
+    imageNames.push(bag);
+}
+console.log(imageNames);
 
 function ChoosenPicCounter(e) {
     e.target;
-    PopUpPic.GetRandomPicIndex();
-    PopUpPic.RenderPics();
-    PopUpPic.ifItHasTheSamePicIndexAndRenderPic();
+    ifItHasTheSamePicIndexAndRenderPic (randomIndex);
+    GetRandomPicIndex ();
+    imgTagGenerator();
     timesClicked++;
     //console.log(timesClicked)
     if (e.target.nodeName !== "IMG") {
@@ -86,8 +84,8 @@ DisplayDiv.addEventListener("click", ChoosenPicCounter);
 
 
 
-    
-    
+
+
 
 
 
