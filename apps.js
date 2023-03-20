f70d02732fbacdac84225863eddd46f378a450a4
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-03-19 19:03:18 -0500

    I added code that would make the images in JavaScript

--- apps.js
+++ apps.js
@@ -7,6 +7,8 @@ let DisplayDiv = document.querySelector("#DisplayImagesDiv");
 let choosenPic = []
 let PicGenerator = function () {
     this.clicks = 0;
+    //this.filePath = filePath;
+    //this.productName = productName;
     this.pic = function () {
         for(let i = 0; i < 3; i++){
             let randomIndex = Math.ceil(Math.random() * 15);
@@ -30,4 +32,23 @@ PopUpPic.pic();
 
 
 
-
+function RenderPics  (){
+    const imagesPath = "./assets";
+    const imageNames = ["bag.jpg", "banana.jpg", "bathroom.jpg", "boots.jpg", "breakfast.jpg","bubblegum.jpg", "chair.jpg", "cthulhu.jpg", "dog-duck.jpg", "dragon.jpg", "pen.jpg", "pet-sweep.jpg", "scissors.jpg", "shark.jpg", "sweep.png", "tauntaun.jpg", "unicorn.jpg", "water-can.jpg", "wine-glass.jpg"];
+    const images = [];
+    //const DisplayDiv = document.querySelector("#DisplayImagesDiv");
+    
+    for (let i = 0; i < imageNames.length; i++) {
+      let imgTag = document.createElement('img');
+      let fullImageTagPath = `${imagesPath}/${imageNames[i]}`;
+      imgTag.setAttribute("src", fullImageTagPath);
+      images.push(imgTag);
+      //DisplayDiv.append(imgTag);
+    }
+    
+    console.log(images);
+    
+    }
+    RenderPics ();
+    //use the images array to work with the loaded images
+    
